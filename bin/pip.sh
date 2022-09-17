831ce397804be3f6bdf97bd7e693cb3dead7a308
zgr2788 <ozgur.beker@gmail.com>
2022-09-17 22:12:20 +0300

    Added write2fastq

--- bin/pip.sh
+++ bin/pip.sh
@@ -4,9 +4,9 @@
 #SBATCH --account=mdbf
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --qos=short_mdbf
-#SBATCH --partition=short_mdbf
-#SBATCH --time=120
+#SBATCH --qos=mid_mdbf
+#SBATCH --partition=mid_mdbf
+#SBATCH --time=1440
 #SBATCH --output=outPip-%j.log
 
 condaDir="Run setup.sh to configure"
